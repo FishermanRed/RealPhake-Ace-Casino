860c78a3947d9c204b39999ea896d370e941d1cd
Red Karam <ReDitto57@gmail.com>
2022-10-27 12:31:35 -0500

    I messed something up I don't understand github

--- RealPhake Ace Casino.cpp
+++ RealPhake Ace Casino.cpp
@@ -52,102 +52,106 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){ //Main 
   switch(msg){
 	case WM_CREATE:
 	  hMenu			= CreateMenu();
-	  hTutorialMenu = CreateMenu();
+	  hTutorialMenu	= CreateMenu();
   
 	  //Primary menu bar
 	  AppendMenu(hMenu, MF_STRING, 1, L"User Info");
-	  AppendMenu(hMenu, MF_POPUP,  (UINT_PTR)hTutorialMenu, L"How to Play");
 	  AppendMenu(hMenu, MF_STRING, 2, L"Card Skins");
-	  AppendMenu(hMenu, MF_STRING, 3, L"About");
-      AppendMenu(hMenu, MF_STRING, 500, L"❡"); //To do: dynamic phake counter
+	  AppendMenu(hMenu, MF_POPUP,  (UINT_PTR)hTutorialMenu, L"How to Play");
+	  AppendMenu(hMenu, MF_STRING, 11, L"About");
+	  AppendMenu(hMenu, MF_STRING, 12, L"❡"); //To do: dynamic phake counter
 
-	  //Tutorial menus
-	  AppendMenu(hTutorialMenu, MF_STRING, 4,  L"How to Play: RealPhake Ace Casino");
+	  //Tutorial options
+	  AppendMenu(hTutorialMenu, MF_STRING, 3,  L"How to Play: RealPhake Ace Casino");
 	  AppendMenu(hTutorialMenu, MF_SEPARATOR,  NULL, NULL);
-	  AppendMenu(hTutorialMenu, MF_STRING, 5,  L"How to Play: Poker");
-	  AppendMenu(hTutorialMenu, MF_STRING, 6,  L"How to Play: Blackjack");
-	  AppendMenu(hTutorialMenu, MF_STRING, 7,  L"How to Play: Tycoon");
-	  AppendMenu(hTutorialMenu, MF_STRING, 8,  L"How to Play: War");
+	  AppendMenu(hTutorialMenu, MF_STRING, 4,  L"How to Play: Poker");
+	  AppendMenu(hTutorialMenu, MF_STRING, 5,  L"How to Play: Blackjack");
+	  AppendMenu(hTutorialMenu, MF_STRING, 6,  L"How to Play: Tycoon");
+	  AppendMenu(hTutorialMenu, MF_STRING, 7,  L"How to Play: War");
 	  AppendMenu(hTutorialMenu, MF_SEPARATOR,  NULL, NULL);
-	  AppendMenu(hTutorialMenu, MF_STRING, 9,  L"How to Play: Roulette");
-	  AppendMenu(hTutorialMenu, MF_STRING, 10, L"How to Play: Craps");
-	  AppendMenu(hTutorialMenu, MF_STRING, 11, L"How to Play: Slots");
+	  AppendMenu(hTutorialMenu, MF_STRING, 8,  L"How to Play: Roulette");
+	  AppendMenu(hTutorialMenu, MF_STRING, 9, L"How to Play: Craps");
+	  AppendMenu(hTutorialMenu, MF_STRING, 10, L"How to Play: Slots");
 	  SetMenu(hWnd, hMenu);
 
 	  //Buttons
-	  CreateWindow(L"BUTTON", L"Play Poker!",	  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 47,  300, 125, 25, hWnd, (HMENU)12, NULL, NULL);
-	  CreateWindow(L"BUTTON", L"Play Blackjack!", WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 182, 300, 125, 25, hWnd, (HMENU)13, NULL, NULL);
-	  CreateWindow(L"BUTTON", L"Play Tycoon!",	  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 317, 300, 125, 25, hWnd, (HMENU)14, NULL, NULL);
-	  CreateWindow(L"BUTTON", L"Play War!",		  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 452, 300, 125, 25, hWnd, (HMENU)15, NULL, NULL);
-	  CreateWindow(L"BUTTON", L"Play Roulette!",  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 115, 335, 125, 25, hWnd, (HMENU)16, NULL, NULL);
-	  CreateWindow(L"BUTTON", L"Play Craps!",	  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 250, 335, 124, 25, hWnd, (HMENU)17, NULL, NULL);
-	  CreateWindow(L"BUTTON", L"Play Slots!",	  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 384, 335, 125, 25, hWnd, (HMENU)18, NULL, NULL);
+	  CreateWindow(L"BUTTON", L"Play Poker!",	  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 47,  300, 125, 25, hWnd, (HMENU)13, NULL, NULL);
+	  CreateWindow(L"BUTTON", L"Play Blackjack!", WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 182, 300, 125, 25, hWnd, (HMENU)14, NULL, NULL);
+	  CreateWindow(L"BUTTON", L"Play Tycoon!",	  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 317, 300, 125, 25, hWnd, (HMENU)15, NULL, NULL);
+	  CreateWindow(L"BUTTON", L"Play War!",		  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 452, 300, 125, 25, hWnd, (HMENU)16, NULL, NULL);
+	  CreateWindow(L"BUTTON", L"Play Roulette!",  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 115, 335, 125, 25, hWnd, (HMENU)17, NULL, NULL);
+	  CreateWindow(L"BUTTON", L"Play Craps!",	  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 250, 335, 124, 25, hWnd, (HMENU)18, NULL, NULL);
+	  CreateWindow(L"BUTTON", L"Play Slots!",	  WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 384, 335, 125, 25, hWnd, (HMENU)19, NULL, NULL);
 	  break;
 	case WM_DESTROY:
 	  PostQuitMessage(0);
 	  break;
 	case WM_COMMAND:
 	  switch(wp){
-		case 500:
-		  MessageBeep(MB_OK);
-		  break;
-
-		case 1: //Make User Info page
+		case 1: //User Info page
 		  if (alreadyOpen == 0){
-			hUserInfo = CreateWindowW(L"userInfoClass", L"User Info", WS_VISIBLE | WS_BORDER | !WS_SIZEBOX, 710, 75, 500, 600, hMainWnd, NULL, NULL, NULL);
+			hUserInfo = CreateWindowW(L"userInfoClass", L"User Info", WS_VISIBLE | WS_BORDER | !WS_SIZEBOX, 710, 75, 500, 600, hMainWnd, NULL, NULL, NULL); //Make User Info window
 			//ShowWindow(hMainWnd, 0); //Uncomment for functioning hide/show
 			alreadyOpen ++;
 		  }
 		  break;
+
 		case 2: //Card Skins page
 		  //To do: Card skins page
 		  break;
-		case 3: //3-11 Tutorial textboxes
-		  MessageBox(hWnd, ABOUT_MENUTEXT, L"About", MB_OK);
-		  break;
-		case 4:
+
+		case 3: //3-10 Tutorial pages
 		  MessageBox(hWnd, RPAC_MENUTEXT, L"How to play RealPhake Ace Casino", MB_OK);
 		  break;
-		case 5:
+		case 4:
 		  MessageBox(hWnd, POKER_MENUTEXT, L"Poker Tutorial", MB_OK);
 		  break;
-		case 6:
+		case 5:
 		  MessageBox(hWnd, PLACEHOLDER, L"Blackjack Tutorial", MB_OK);
 		  break;
-		case 7:
+		case 6:
 		  MessageBox(hWnd, PLACEHOLDER, L"Tycoon Tutorial", MB_OK);
 		  break;
-		case 8:
+		case 7:
 		  MessageBox(hWnd, PLACEHOLDER, L"War Tutorial", MB_OK);
 		  break;
-		case 9:
+		case 8:
 		  MessageBox(hWnd, PLACEHOLDER, L"Roulette Tutorial", MB_OK);
 		  break;
-		case 10:
+		case 9:
 		  MessageBox(hWnd, PLACEHOLDER, L"Craps Tutorial", MB_OK);
 		  break;
-		case 11:
+		case 10:
 		  MessageBox(hWnd, PLACEHOLDER, L"Slots Tutorial", MB_OK);
 		  break;
-		case 12: //12-18 The games
+
+		case 11: //About page
+		  MessageBox(hWnd, ABOUT_MENUTEXT, L"About", MB_OK);
+		  break;
+
+		case 12: //Phake
+		  MessageBeep(MB_OK);
+		  break;
+
+		case 13: //13-19 The games
 		  //To do: Add Poker
 		  break;
-		case 13:
+		case 14:
 		  //To do: Add Blackjack
 		  break;
-		case 14:
+		case 15:
 		  //To do: Add Tycoon
 		  break;
-		case 15:
+		case 16:
 		  //To do: Add War
 		  break;
-		case 16:
+		case 17:
 		  //To do: Add Roulette
 		  break;
-		case 17:
+		case 18:
 		  //To do: Add Craps
 		  break;
-		case 18:
+		case 19:
 		  //To do: Add Slots
 		  break;
 	  }
